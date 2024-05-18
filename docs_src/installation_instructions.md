d8ae1e7f2d27448122b4251165e50e72f6c8376f
Nigel van Keulen <nigel2392@gmail.com>
2024-05-18 18:10:45 +0200

    Include adapter_help command output in docs

--- docs_src/installation_instructions.md
+++ docs_src/installation_instructions.md
@@ -15,3 +15,6 @@ Getting Started
 3. Run `py ./manage.py adapter_help` to see all your options and their requirements.
 
 Click [here]({{ index .Tree.make_editable.URL }}) to get more information on how to make your models editable.
+
+Adapter help output:
+```{{ Exec "py './wagtail_fedit/test/manage.py' adapter_help" }}```
--- wagtail_fedit/test/testapp/settings.py
+++ wagtail_fedit/test/testapp/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+WAGTAILADMIN_BASE_URL = '/admin/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
